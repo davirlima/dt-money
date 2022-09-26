159ac797bece2aa9f2b5aa4423ec73808a39e000
Davi Lima <davi.rlima04@gmail.com>
2022-09-26 13:23:56 -0400

    componente header

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,14 @@
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/themes/default";
 import { GlobalStyle } from "./styles/global";
+import { Transactions } from "./pages/Transactions";
 
 export function App() {
   return (
     <div>
       <ThemeProvider theme={defaultTheme}>
         <GlobalStyle />
-        <h1>Teste</h1>
-        <p>Teste</p>
+        <Transactions />
       </ThemeProvider>
     </div>
   );
